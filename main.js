c8558bb9a05772fa3daf1f2ca842bc80d3bd14ca
adityadaffaaa <adityadaffafawwaz@gmail.com>
2023-01-17 12:41:37 +0700

    Buat ayang

---
+++ main.js
@@ -0,0 +1,43 @@
+var kelopak1 = document.querySelector(".kelopak-1");
+setTimeout(() => {
+  kelopak1.style.transform = "scale(100%)";
+}, 1900);
+var kelopak2 = document.querySelector(".kelopak-2");
+setTimeout(() => {
+  kelopak2.style.transform = "scale(100%)";
+  kelopak2.style.transform = "rotate(45deg)";
+}, 2200);
+var kelopak3 = document.querySelector(".kelopak-3");
+setTimeout(() => {
+  kelopak3.style.transform = "scale(100%)";
+  kelopak3.style.transform = "rotate(90deg)";
+}, 2500);
+var kelopak4 = document.querySelector(".kelopak-4");
+setTimeout(() => {
+  kelopak4.style.transform = "scale(100%)";
+  kelopak4.style.transform = "rotate(120deg)";
+}, 2800);
+var kelopak5 = document.querySelector(".kelopak-5");
+setTimeout(() => {
+  kelopak5.style.transform = "scale(100%)";
+  kelopak5.style.transform = "rotate(180deg)";
+}, 3100);
+var kelopak6 = document.querySelector(".kelopak-6");
+setTimeout(() => {
+  kelopak6.style.transform = "scale(100%)";
+  kelopak6.style.transform = "rotate(-120deg)";
+}, 3400);
+var kelopak7 = document.querySelector(".kelopak-7");
+setTimeout(() => {
+  kelopak7.style.transform = "scale(100%)";
+  kelopak7.style.transform = "rotate(-90deg)";
+}, 3700);
+var kelopak8 = document.querySelector(".kelopak-8");
+setTimeout(() => {
+  kelopak8.style.transform = "scale(100%)";
+  kelopak8.style.transform = "rotate(-45deg)";
+}, 4000);
+var wrap = document.querySelector(".wrap");
+setTimeout(() => {
+  wrap.style.filter = "drop-shadow(0px 0px 28px #c7009b)";
+}, 4000);
